03b045cf8a814b165b4db36e801006f40d601a3e
Alex Jose Loja Zumaeta <55153136+aljozu@users.noreply.github.com>
2021-11-25 00:12:30 -0500

    Update lab10.py

--- lab10.py
+++ lab10.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[12]:
 
 
 import os
 import face_recognition
 
 
-# In[3]:
+# In[29]:
 
 
 rootdir = 'C:\\Users\\lojaz\\Desktop\\script\\lfw'
@@ -26,27 +26,27 @@ for subdir, dirs, files in os.walk(rootdir):
         break
 
 
-# In[4]:
+# In[14]:
 
 
 from random import randint
 
 
-# In[5]:
+# In[15]:
 
 
 p1 = face_recognition.face_distance(puntos, puntos[randint(0,50)])
 p2 = face_recognition.face_distance(puntos, puntos[randint(0,50)])
 
 
-# In[6]:
+# In[16]:
 
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-# In[7]:
+# In[17]:
 
 
 f1=list(np.append(p1,p2))
@@ -54,13 +54,13 @@ plt.hist(f1)
 plt.show()
 
 
-# In[8]:
+# In[22]:
 
 
 from rtree import index
 
 
-# In[9]:
+# In[32]:
 
 
 p = index.Property()
@@ -71,9 +71,45 @@ p.idx_extension = 'index'
 idx = index.Index('128d_index', properties=p)
 
 
-# In[10]:
+# In[33]:
 
 
 for i in range(0, len(puntos)):
     idx.insert(i, tuple(puntos[i])*2)
+
+
+# In[35]:
+
+
+#Range Search
+def range_search(img, r):
+    picture = face_recognition.load_image_file(img)
+    known_face_encoding = face_recognition.face_encodings(picture)[0]
+    ans = []
+    for i in puntos:
+        if len(face_recognition.face_encodings(i))>0:
+            image_compare_encoding = face_recognition.face_encodings(i)[0]
+            dist = face_recognition.face_distance(known_face_encoding, image_compare_encoding)
+            if dist < r:
+                result.append(i)
+    return result
+
+
+# In[ ]:
+
+
+#KNN Search
+def KNN_search(img, k):
+    picture = face_recognition.load_image_file(img)
+    known_face_encoding = face_recognition.face_encodings(picture)[0]
+    ans = PriorityQueue()
+    for i in puntos:
+        if len(face_recognition.face_encodings(i))>0:
+            image_compare_encoding = face_recognition.face_encodings(i)[0]
+            dist = face_recognition.face_distance(known_face_encoding, image_compare_encoding)
+            ans.put((dist, i))
+    final_ans = []
+    for i in range(k):
+        final_ans.append(result.get())
+    return final_ans
 
